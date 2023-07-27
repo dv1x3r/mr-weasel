5940c22e5c13c44af6fcb6b5eac5d1397530a1e1
dv1x3r <d.purvinsh@gmail.com>
2023-07-27 03:14:24 +0300

    car notes

--- commands/car/car.go
+++ commands/car/car.go
@@ -20,3 +20,30 @@ func (CarCommand) ExecuteTelegram(pl tg.Payload) (tg.Result, error) {
 	}
 	return tg.Result{Text: ".", Keyboard: keyboard}, nil
 }
+
+/*
+
+/car
+
+Traveled in the past year: 10,000 KM (+2%)
+Gas Expenses: 1000 EUR (+10%)
+Other Expenses: 500 EUR (-10%)
+License expiration: 01-Jan-2099
+
+Please select your car for additional actions:
+
+| Lexus IS250 (2011) | BMW 520i (2021) |
+| Previous | Next |
+
+
+/car 1
+
+Car name: BMW 520i (2021)
+Traveled in the past year: 10,000 KM (+2%)
+Gas Expenses: 1000 EUR (+10%)
+Other Expenses: 500 EUR (-10%)
+
+| Add Gas | Add Service | Update Attributes
+| Edit Gas | Edit Service |
+
+*/
