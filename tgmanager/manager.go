1f849057cfaea73f877ed5f7feef93187e607096
dv1x3r <d.purvinsh@gmail.com>
2023-08-14 13:10:20 +0300

    split tg manager

--- tgmanager/manager.go
+++ tgmanager/manager.go
@@ -47,76 +47,85 @@ func (m *Manager) PublishCommands() {
 }
 
 func (m *Manager) Start() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	cfg := tgclient.GetUpdatesConfig{
 		Offset:         -1,
 		Timeout:        60,
 		AllowedUpdates: []string{"message", "callback_query"},
 	}
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	updates := m.client.GetUpdatesChan(ctx, cfg, 100)
 	for update := range updates {
-		if update.Message != nil && update.Message.From != nil {
-			pl := commands.Payload{UserID: update.Message.From.ID, Command: update.Message.Text}
-			res, err := m.executeCommand(ctx, pl)
-			if err != nil {
-				log.Println("[ERROR]", err)
-			} else {
-				// If it is normal message, user can change and escape states
-				m.setState(pl.UserID, res.State, true)
-			}
+		m.processUpdate(ctx, update)
+	}
+}
 
-			if res.Text != "" {
-				_, err = m.client.SendMessage(ctx, tgclient.SendMessageConfig{
-					ChatID:      update.Message.Chat.ID,
-					Text:        res.Text,
-					ReplyMarkup: res.Keyboard,
-				})
-				if err != nil {
-					log.Println("[ERROR]", err)
-				}
-			}
+func (m *Manager) processUpdate(ctx context.Context, update tgclient.Update) {
+	if update.Message != nil && update.Message.From != nil {
+		m.processMessage(ctx, *update.Message)
+	} else if update.CallbackQuery != nil {
+		m.processCallbackQuery(ctx, *update.CallbackQuery)
+	}
+}
 
-		} else if update.CallbackQuery != nil {
-			pl := commands.Payload{UserID: update.CallbackQuery.From.ID, Command: update.CallbackQuery.Data}
-			res, err := m.executeCommand(ctx, pl)
-			if err != nil {
-				log.Println("[ERROR]", err)
-			} else {
-				// If it is callback event, user can change state only
-				// If it is callback event, user can't clear his current state
-				m.setState(pl.UserID, res.State, false)
-			}
+func (m *Manager) processMessage(ctx context.Context, message tgclient.Message) {
+	pl := commands.Payload{UserID: message.From.ID, Command: message.Text}
+	res, err := m.executeCommand(ctx, pl)
+	if err != nil {
+		log.Println("[ERROR]", err)
+	} else {
+		// If it is normal message, user can change and escape states
+		m.setState(pl.UserID, res.State, true)
+	}
+	if res.Text == "" {
+		return
+	}
+	_, err = m.client.SendMessage(ctx, tgclient.SendMessageConfig{
+		ChatID:      message.Chat.ID,
+		Text:        res.Text,
+		ReplyMarkup: res.Keyboard,
+	})
+	if err != nil {
+		log.Println("[ERROR]", err)
+	}
+}
 
-			_, err = m.client.AnswerCallbackQuery(ctx, tgclient.AnswerCallbackQueryConfig{
-				CallbackQueryID: update.CallbackQuery.ID,
-			})
-			if err != nil {
-				log.Println("[ERROR]", err)
-			}
-
-			if res.Text != "" && res.Keyboard != nil {
-				_, err = m.client.EditMessageText(ctx, tgclient.EditMessageTextConfig{
-					ChatID:      update.CallbackQuery.Message.Chat.ID,
-					MessageID:   update.CallbackQuery.Message.MessageID,
-					Text:        res.Text,
-					ReplyMarkup: res.Keyboard,
-				})
-				if err != nil {
-					log.Println("[ERROR]", err)
-				}
-			} else if res.Text != "" {
-				_, err = m.client.SendMessage(ctx, tgclient.SendMessageConfig{
-					ChatID:      update.CallbackQuery.Message.Chat.ID,
-					Text:        res.Text,
-					ReplyMarkup: res.Keyboard,
-				})
-				if err != nil {
-					log.Println("[ERROR]", err)
-				}
-			}
-
+func (m *Manager) processCallbackQuery(ctx context.Context, callbackQuery tgclient.CallbackQuery) {
+	pl := commands.Payload{UserID: callbackQuery.From.ID, Command: callbackQuery.Data}
+	res, err := m.executeCommand(ctx, pl)
+	if err != nil {
+		log.Println("[ERROR]", err)
+	} else {
+		// If it is callback event, user can change state only
+		// If it is callback event, user can't clear his current state
+		m.setState(pl.UserID, res.State, false)
+	}
+	_, err = m.client.AnswerCallbackQuery(ctx, tgclient.AnswerCallbackQueryConfig{CallbackQueryID: callbackQuery.ID})
+	if err != nil {
+		log.Println("[ERROR]", err)
+	}
+	if res.Text == "" {
+		return
+	}
+	if res.Keyboard != nil {
+		_, err = m.client.EditMessageText(ctx, tgclient.EditMessageTextConfig{
+			ChatID:      callbackQuery.Message.Chat.ID,
+			MessageID:   callbackQuery.Message.MessageID,
+			Text:        res.Text,
+			ReplyMarkup: res.Keyboard,
+		})
+		if err != nil {
+			log.Println("[ERROR]", err)
+		}
+	} else {
+		_, err = m.client.SendMessage(ctx, tgclient.SendMessageConfig{
+			ChatID:      callbackQuery.Message.Chat.ID,
+			Text:        res.Text,
+			ReplyMarkup: res.Keyboard,
+		})
+		if err != nil {
+			log.Println("[ERROR]", err)
 		}
 	}
 }
