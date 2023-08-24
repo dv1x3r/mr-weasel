15bfeea68e444659be48416b236e6653f13a2b6b
dv1x3r <d.purvinsh@gmail.com>
2023-08-25 02:49:07 +0300

    holiday queries

--- storage/holiday.go
+++ storage/holiday.go
@@ -23,6 +23,11 @@ type HolidayBase struct {
 	Days   int64 `db:"days"`
 }
 
+type HolidayDaysByYear struct {
+	Year int64 `db:"year"`
+	Days int64 `db:"days"`
+}
+
 func (h *HolidayBase) GetStartTimestamp() string {
 	return time.Unix(h.Start, 0).UTC().Format("Monday, 02 January 2006")
 }
@@ -31,14 +36,27 @@ func (h *HolidayBase) GetEndTimestamp() string {
 	return time.Unix(h.End, 0).UTC().Format("Monday, 02 January 2006")
 }
 
-func (s *CarStorage) SelectHolidaysFromDB(ctx context.Context, userID int64) ([]HolidayBase, error) {
-	var holidays []HolidayBase
+func (s *HolidayStorage) SelectHolidayDaysByYearFromDB(ctx context.Context, userID int64) ([]HolidayDaysByYear, error) {
+	var holidays []HolidayDaysByYear
 	stmt := `
-		select id, user_id, start, end, days
+		select year(start) as year, sum(days) as days
 		from holiday
 		where user_id = ?
-		order by start;
+		order by 1;
 	`
 	err := s.db.SelectContext(ctx, &holidays, stmt, userID)
 	return holidays, err
+}
+
+func (s *CarStorage) GetHolidayFromDB(ctx context.Context, userID int64, offset int64) (HolidayBase, error) {
+	var holiday HolidayBase
+	stmt := `
+		select id, user_id, start, end, days
+		from holidays
+		where user_id = ?
+		order by start desc
+		limit 1 offset ?;
+	`
+	err := s.db.GetContext(ctx, &holiday, stmt, userID, offset)
+	return holiday, err
 }
