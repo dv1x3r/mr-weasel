c8424ba335f87c53f61a35c7201c55a70677f810
dv1x3r <d.purvinsh@gmail.com>
2023-08-25 12:57:28 +0300

    show holiday days init

--- commands/holiday.go
+++ commands/holiday.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"context"
+	"fmt"
 
 	st "mr-weasel/storage"
 )
@@ -24,4 +25,25 @@ func (HolidayCommand) Description() string {
 
 func (c *HolidayCommand) Execute(ctx context.Context, pl Payload) (Result, error) {
 	return Result{}, nil
+}
+
+func (c *HolidayCommand) showHolodayDaysByYear(ctx context.Context, userID int64) (Result, error) {
+	holidays, err := c.storage.SelectHolidayDaysByYearFromDB(ctx, userID)
+	if err != nil {
+		return Result{Text: "There is something wrong, please try again."}, err
+	}
+
+	res := Result{}
+	if len(holidays) == 0 {
+		res.Text = "Holidays not found."
+	} else {
+		res.Text = "Holiday days by year: \n"
+		for _, v := range holidays {
+			res.Text += fmt.Sprintf("%d - %d days\n", v.Year, v.Days)
+		}
+	}
+
+	res.AddKeyboardButton("Show details", "-")
+
+	return res, nil
 }
