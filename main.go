b6c680c2f6a933d98c321b3efb5978d5d5258aeb
dv1x3r <d.purvinsh@gmail.com>
2023-09-16 04:25:54 +0300

    fix signal ch buffer

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 
 func mainContext() context.Context {
 	ctx, cancel := context.WithCancel(context.Background())
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		select {
